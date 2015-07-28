b4085635330798c507d4eb8d4e53be52291f2c8b
Darin Jacobson <djake1680@gmail.com>
2015-07-28 09:50:55 -0700

    took pic out

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+var y;
 
 $(document).ready(function(){
 	$(".gameboard").addClass("container col-xs-8");
@@ -9,7 +9,6 @@ $(document).ready(function(){
 	 for (var x = 0; x < 9; x) {
 	 for (var i = x; i < x + 3; i++){
 	 	$('<div class="select_box col-xs-4"></div>').appendTo(".game");
-
 	 	console.log(i);
 	 	
 
